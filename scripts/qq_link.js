18dd80e0c7cb9612488bbeedff2c2242ffd4504a
Loukyuu <29080642+Loukyuu1120@users.noreply.github.com>
2024-12-09 13:02:47 +0800

    Update qq_link.js

--- scripts/qq_link.js
+++ scripts/qq_link.js
@@ -5,7 +5,7 @@ const rules = [
     { regex: /^https:\/\/c\.pc\.qq\.com\/index\.html\?pfurl=(http.*)&pfuin=.*/, url: '$1' },
     { regex: /^https:\/\/pingtas\.qq\.com\/webview\/pingd\?dm=c\.pc\.qq\.com&pvi=\d+&si=s\d+&url=\/middlem\.html\?pfurl%3d(http.*)%26pfuin%3d.*%26pfuin%3d.*/, url: '$1' },
     { regex: /^https:\/\/cgi\.connect\.qq\.com\/qqconnectopen\/get_urlinfoForQQV2\?url=(http.*)/, url: '$1' },
-    { regex: /^https:\/\/c\.pc\.qq\.com\/ios\.html\?.*url=(http.*)&(sub)?level=.*/,url: (match, url) => url.replace(/%2f$/, '') }
+    { regex: /^https:\/\/c\.pc\.qq\.com\/ios\.html\?.*url=(http.*)&(sub)?level=.*/, url: '$1' }
 ];
 
 const requestUrl = $request.url;
@@ -14,7 +14,8 @@ for (let rule of rules) {
     if (rule.regex.test(requestUrl)) {
         const match = requestUrl.match(rule.regex);
         if (match && match[1]) {
-            const decodedUrl = decodeURIComponent(match[1]);
+            let decodedUrl = decodeURIComponent(match[1]);
+            decodedUrl = decodedUrl.replace(/(%2f|\/)+$/i, '');
             $done({ response: { status: 302, headers: { Location: decodedUrl } } });
         }
     }
