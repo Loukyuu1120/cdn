b5095fba1ba4224848d2f2ea09890dc066dad470
Loukyuu <29080642+Loukyuu1120@users.noreply.github.com>
2024-12-09 12:15:36 +0800

    Update qq_link.js

--- scripts/qq_link.js
+++ scripts/qq_link.js
@@ -5,7 +5,7 @@ const rules = [
     { regex: /^https:\/\/c\.pc\.qq\.com\/index\.html\?pfurl=(http.*)&pfuin=.*/, url: '$1' },
     { regex: /^https:\/\/pingtas\.qq\.com\/webview\/pingd\?dm=c\.pc\.qq\.com&pvi=\d+&si=s\d+&url=\/middlem\.html\?pfurl%3d(http.*)%26pfuin%3d.*%26pfuin%3d.*/, url: '$1' },
     { regex: /^https:\/\/cgi\.connect\.qq\.com\/qqconnectopen\/get_urlinfoForQQV2\?url=(http.*)/, url: '$1' },
-    { regex: /^https:\/\/c\.pc\.qq\.com\/ios\.html\?url=(http.*)&level=.*/, url: '$1' }
+    { regex: /^https:\/\/c\.pc\.qq\.com\/ios\.html\?.*url=(http.*)&(sub)?level=.*/, url: '$1' }
 ];
 
 const requestUrl = $request.url;
